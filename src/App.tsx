dae1acca3a3114724eeedcaab1daa1bd49d0f874
HugoZAuad <hugozeymer@hotmail.com>
2025-04-09 17:31:35 -0300

    Feito alteração

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,103 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-interface TodoItem{
-  id: string,
-  texto: string,
-  completado: boolean
+interface TodoItem {
+  id: string;
+  texto: string;
+  completado: boolean;
 }
 
 function App() {
-  const [todos, setTodos] = useState<TodoItem[]>([])
-  const [novoTodo, setNovoTodo] = useState<string>("")
-  const [estaCarregado, setEstaCarregado] = useState<boolean>(false)
+  const chaveTarefasMemoria = "tarefas"
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [novoTodo, setNovoTodo] = useState<string>("");
+  const [estaCarregado, setEstaCarregado] = useState<boolean>(false);
 
   const adicionarTarefa = (): void => {
-    if(novoTodo !== "" ){
-      const newId = crypto.randomUUID()
+    if (novoTodo !== "") {
+      const newId = crypto.randomUUID();
       const newTodoItem: TodoItem = {
         id: newId,
         texto: novoTodo,
-        completado: false
-      }
-      setTodos([...todos, newTodoItem])
-      setNovoTodo("")
+        completado: false,
+      };
+      setTodos([...todos, newTodoItem]);
+      setNovoTodo("");
     }
-  }
+  };
 
-  const removerTarefa = (id:string): void => {
-    const tarefasAtualizadas = todos.filter(todo => todo.id !== id)
-    setTodos(tarefasAtualizadas)
-  }
+  const removerTarefa = (id: string): void => {
+    const tarefasAtualizadas = todos.filter((todo) => todo.id !== id);
+    setTodos(tarefasAtualizadas);
+  };
 
   const marcarCompleto = (id: string): void => {
     const todosAtualizado = todos.map((todo) => {
-      if (todo.id === id){
-        return{ ...todo, completado: !todo.completado }
+      if (todo.id === id) {
+        return { ...todo, completado: !todo.completado };
       }
-      return todo
-    })
-    setTodos(todosAtualizado)
-  }
+      return todo;
+    });
+    setTodos(todosAtualizado);
+  };
 
   const obterTarefasCompletas = (): TodoItem[] => {
-    return todos.filter(todo => todo.completado)
-  }
+    return todos.filter((todo) => todo.completado);
+  };
+
+  useEffect(() => {
+    if (estaCarregado) {
+      localStorage.setItem(chaveTarefasMemoria, JSON.stringify(todos));
+    }
+  }, [todos, estaCarregado]);
+
+  useEffect(() => {
+    const tarefasDaMemoria = localStorage.getItem(chaveTarefasMemoria);
+
+    if (tarefasDaMemoria) {
+      setTodos(JSON.parse(tarefasDaMemoria));
+    }
+
+    setEstaCarregado(true);
+
+  }, []);
 
   return (
-    <div className='app'>
-      <div className='container'>
-        <h1>Lista de tarefas - {obterTarefasCompletas().length} / {todos.length}</h1>
-        <div className='input-container'>
-          <input type="text" value={novoTodo} onChange={(e) => setNovoTodo(e.target.value)} />
+    <div className="app">
+      <div className="container">
+        <h1>
+          Lista de tarefas - {obterTarefasCompletas().length} / {todos.length}
+        </h1>
+        <div className="input-container">
+          <input
+            type="text"
+            value={novoTodo}
+            onChange={(e) => setNovoTodo(e.target.value)}
+          />
           <button onClick={adicionarTarefa}>Adicionar Tarefa</button>
         </div>
         <ol>
-          {
-            todos.map((todo)=> (
-              <li key={todo.id}>
-                <input type="checkbox" checked={todo.completado} onChange={() => marcarCompleto(todo.id)} />
-                <span style={{ textDecoration: todo.completado ? 'line-through' : 'none'} }>{todo.texto}</span>
-                <button onClick={() => removerTarefa(todo.id)}> Remover</button>
-              </li>
-
-            ))  
-          }
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completado}
+                onChange={() => marcarCompleto(todo.id)}
+              />
+              <span
+                style={{
+                  textDecoration: todo.completado ? "line-through" : "none",
+                }}
+              >
+                {todo.texto}
+              </span>
+              <button onClick={() => removerTarefa(todo.id)}> Remover</button>
+            </li>
+          ))}
         </ol>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
